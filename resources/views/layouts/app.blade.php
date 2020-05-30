c2ee5ba7494a05fde34acdace3b23ce498cf61ba
iagoMAO <iagologo1@gmail.com>
2020-05-30 12:43:13 -0400

    Navbar & some frontpage stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,63 +18,60 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/global.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <div class="justify-content-center" id="app">
+        <div class="page-content ml-auto mr-auto py-2">
+            <nav class="nav-nav">
+                <div class="banner">
+                    <div class="justify-content-center text-center nav-play">
+                        <a href="/register">
+                            <img class="nav-logo-rbx" src="{{asset('/img/res/Holiday3Button.png')}}">
+                        </a>
+                    </div>
+                    <div class="nav-options">
+                        <a href="/login" class="p-1 nav-options-link">Login</a>
+                    </div>
+                    <div class="nav-logo ml-auto mr-auto text-center">
+                        <a href="/">
+                            <img src="{{asset('/img/res/roblox_logo.png')}}" class="nav-logo-rbx">
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </nav>
+                <div class="nav-navbar">
+                    <div class="nav-links px-5">
+                        <div class="nav-link p-2">
+                            <a href="/home">
+                                My {{config('app.name')}}
+                            </a>
+                        </div>
+                        <span class="nav-seperator">|</span>
+                        <div class="nav-link p-2">
+                            <a href="/games">
+                                Games
+                            </a>
+                        </div>
+                        <span class="nav-seperator">|</span>
+                        <div class="nav-link p-2">
+                            <a href="/catalog">
+                                Catalog
+                            </a>
+                        </div>
+                        <span class="nav-seperator">|</span>
+                        <div class="nav-link p-2">
+                            <a href="/browse">
+                                People
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            <main class="content py-3">
+                @yield('content')
+            </main>
+        </div>
     </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,47 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@section('content')
+<div class="container">
+    <div class="row justify-content-left pb-2">
+        <div class="col-auto p-0">
+            <div id="login-box" class="box-container">
+                <h5 class="box-header">
+                    Member Login
+                </h5>
             </div>
         </div>
-    </body>
-</html>
+        <div class="col-auto px-2">
+            <div id="trailer-box" class="box-container">
+                <embed type="application/x-shockwave-flash" src="http://noobtube.asia/2009/player.swf?autoplay=0" style="" id="movie_player" name="movie_player" bgcolor="#000000" quality="high" allowfullscreen="true" allowscriptaccess="always" flashvars="vq=null&amp;video_id=frog&amp;l=167&amp;sk=Vnur-Y_v1_ydWWGwFDo-lWRFyyrJDLQsC&amp;fmt_map=6/720000/7/0/0&amp;t=OEgsToPDskIaTZRI6WGtBuV7RsPGLEwI&amp;hl=en&amp;plid=AART2Y-pHEmXDvDEAAAAoIQ8YAA&amp;playnext=0&amp;enablejsapi=1" width="426" height="252">
+            </div>
+        </div>
+        <div class="col-auto p-0">
+            <div id="square-ad" class="box-container">
+                <img src="{{asset('/img/res/frog.jpg')}}">
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-left">
+        <div class="col-auto p-0">
+            <a href="/register">
+                <div id="playnow-ad" class="box-container">
+
+                </div>
+            </a>
+            <div id="featured-place" class="box-container my-2">
+
+            </div>
+        </div>
+        <div class="col-auto p-0 ml-2">
+            <div id="roblox-facts" class="box-container">
+                <h5>
+                    {{config('app.name')}} Facts
+                </h5>
+                <p>1 - we are awesome</p>
+                <p>2 - we are awesome</p>
+                <p>3 - we are awesome</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
